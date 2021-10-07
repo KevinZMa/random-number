4ec7c2ecd780bafa155a736fb41d46e0fdaa09da
KevinZMa <66984843+KevinZMa@users.noreply.github.com>
2021-10-06 17:50:22 -0700

    chore: prettier

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     try {
         res.json({
             status: 200,
-            number: random(1, 1000)
-        })
+            number: random(1, 1000),
+        });
     } catch (err) {
         console.error(err);
-        res.status(500).send('An internal server error occured.');
+        res.status(500).send("An internal server error occured.");
     }
 });
 
