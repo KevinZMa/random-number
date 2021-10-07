c19980abd9e092954f07193112161bc413f6bfd5
KevinZMa <66984843+KevinZMa@users.noreply.github.com>
2021-10-06 17:37:18 -0700

    feat(*): initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+app.get('/', (req, res) => {
+    try {
+        res.json({
+            status: 200,
+            number: random()
+        })
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('An internal server error occured.');
+    }
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
