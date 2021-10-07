db922ba24e61bbf9fe9a97a5d6444220feee9ad6
KevinZMa <66984843+KevinZMa@users.noreply.github.com>
2021-10-06 17:59:41 -0700

    feat(api): min and max query params

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 app.get("/", (req, res) => {
     try {
+        const min = parseInt(req.query.min) || 1,
+            max = parseInt(req.query.max) || 1000;
         res.json({
             status: 200,
-            number: random(1, 1000),
+            number: random(min, max),
         });
     } catch (err) {
         console.error(err);
